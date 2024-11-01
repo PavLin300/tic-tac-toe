db3d299a33bd3f1b4b199617de94cf6d6c660a5a
Pasha <p.buriak2005@gmail.com>
2024-11-01 12:43:36 +0200

    style with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import { Col, Container, Row } from "react-bootstrap";
 function Square({ value, onSquareClick }) {
 	return (
-		<button className='square' onClick={onSquareClick}>
+		<Button
+			onClick={onSquareClick}
+			style={{ width: 100, height: 100 }}
+			variant='light'
+			className='border border-primary fs-4'
+		>
 			{value}
-		</button>
+		</Button>
 	);
 }
 
@@ -32,42 +39,40 @@ function Board({ xIsNext, squares, onPlay }) {
 
 	return (
 		<>
-			<div className='status'>{status}</div>
-			<div className='board-row'>
+			<div className='mb-2 fs-2 '>{status}</div>
+			<Row>
 				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
 				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-			</div>
-			<div className='board-row'>
+			</Row>
+			<Row>
 				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
 				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
 				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-			</div>
-			<div className='board-row'>
+			</Row>
+			<Row>
 				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
 				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
 				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-			</div>
+			</Row>
 		</>
 	);
 }
 
 export default function Game() {
-	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
 	const [currentMove, setCurrentMove] = useState(0);
+	const xIsNext = currentMove % 2 === 0;
 	const currentSquares = history[currentMove];
 
 	function handlePlay(nextSquares) {
 		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
 		setHistory(nextHistory);
 		setCurrentMove(nextHistory.length - 1);
-		setXIsNext(!xIsNext);
 	}
 
 	function jumpTo(nextMove) {
 		setCurrentMove(nextMove);
-		setXIsNext(nextMove % 2 === 0);
 	}
 
 	const moves = history.map((squares, move) => {
@@ -79,20 +84,34 @@ export default function Game() {
 		}
 		return (
 			<li key={move}>
-				<button onClick={() => jumpTo(move)}>{description}</button>
+				<Button
+					variant='primary'
+					onClick={() => jumpTo(move)}
+					className='w-25 m-1 d-block'
+				>
+					{description}
+				</Button>
 			</li>
 		);
 	});
 
 	return (
-		<div className='game'>
-			<div className='game-board'>
-				<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-			</div>
-			<div className='game-info'>
-				<ol>{moves}</ol>
-			</div>
-		</div>
+		<Container>
+			<Row>
+				<Col className='d-flex justify-content-center'>
+					<div>
+						<Board
+							xIsNext={xIsNext}
+							squares={currentSquares}
+							onPlay={handlePlay}
+						/>
+					</div>
+				</Col>
+				<Col className='pt-5'>
+					<ol>{moves}</ol>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
 
